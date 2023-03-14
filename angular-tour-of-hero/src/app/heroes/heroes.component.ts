b6367199d39ddc21a998baf10724a0e60082ca68
Jason Wang <jason.w@ibm.com>
2023-03-14 09:56:51 +0800

    新增service

--- angular-tour-of-hero/src/app/heroes/heroes.component.ts
+++ angular-tour-of-hero/src/app/heroes/heroes.component.ts
@@ -1,7 +1,7 @@
+import { MessageService } from './../messages.service';
 import { Component, OnInit } from '@angular/core';
-import { FormsModule } from '@angular/forms';
 import { Hero } from '../hero';
-import { HEROES } from '../mock-heroes';
+import { HeroService } from '../hero.service';
 
 @Component({
   selector: 'app-heroes',
@@ -10,16 +10,25 @@ import { HEROES } from '../mock-heroes';
 })
 export class HeroesComponent implements OnInit {
 
-  public heroes = HEROES;
+  public heroes:Hero[] = [];
   public selectedHero?: Hero;
 
-  constructor() { }
+  constructor(
+    private heroService: HeroService,
+    private messageService: MessageService
+  ) { }
 
   ngOnInit() {
+    this.getHeroes();
   }
 
   onSelect(hero: Hero) {
     this.selectedHero = hero;
+    this.messageService.add(`HeroesComponent: Selected hero id=${hero.id}`);
+  }
+
+  getHeroes(): void {
+    this.heroService.getHeroes().subscribe(heros => this.heroes = heros);
   }
 
 }
